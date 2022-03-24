decbb4d66ba8d93cc4f47b11e2fbf7dbd5372e4f
saityan <naminatosky@gmail.com>
2022-03-24 14:48:45 +0300

    new MainActivity espresso tests

--- app/src/androidTest/java/com/geekbrains/tests/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/MainActivityEspressoTest.kt
@@ -1,6 +1,8 @@
 package com.geekbrains.tests
 
 import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
@@ -10,6 +12,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.geekbrains.tests.view.search.MainActivity
+import junit.framework.TestCase
 import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
@@ -27,20 +30,56 @@ class MainActivityEspressoTest {
     }
 
     @Test
-    fun activitySearch_IsWorking() {
-        onView(withId(R.id.searchEditText)).perform(click())
-        onView(withId(R.id.searchEditText)).perform(replaceText("algol"), closeSoftKeyboard())
-        onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
+    fun activity_IsResumed() {
+        TestCase.assertEquals(Lifecycle.State.RESUMED, scenario.state)
+    }
 
-        if (BuildConfig.TYPE == MainActivity.RELEASE) {
-            onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 42")))
-        } else {
-            onView(isRoot()).perform(delay())
-            onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 2283")))
+    @Test
+    fun activityTextView_NotNull() {
+        scenario.onActivity {
+            val totalCountTextView = it.findViewById<TextView>(R.id.totalCountTextView)
+            TestCase.assertNotNull(totalCountTextView)
         }
     }
 
-    private fun delay(): ViewAction? {
+    @Test
+    fun activityEditText_NotNull() {
+        scenario.onActivity {
+            val searchEditText = it.findViewById<TextView>(R.id.searchEditText)
+            TestCase.assertNotNull(searchEditText)
+        }
+    }
+
+    @Test
+    fun activityButton_NotNull() {
+        scenario.onActivity {
+            val toDetailsActivityButton = it.findViewById<TextView>(R.id.toDetailsActivityButton)
+            TestCase.assertNotNull(toDetailsActivityButton)
+        }
+    }
+
+    @Test
+    fun activityTextView_HasText() {
+        val assertion = matches(withText("TO DETAILS"))
+        onView(withId(R.id.toDetailsActivityButton)).check(assertion)
+    }
+
+    @Test
+    fun activityEditText_HasText() {
+        val assertion = matches(withText(""))
+        onView(withId(R.id.searchEditText)).check(assertion)
+    }
+
+    @Test
+    fun activitySearch_IsWorking() {
+        onView(withId(R.id.searchEditText)).perform(click())
+        onView(withId(R.id.searchEditText)).perform(replaceText("test"), closeSoftKeyboard())
+        onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
+        onView(isRoot()).perform(delay())
+        onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 14")))
+    }
+
+    private fun delay(): ViewAction {
         return object : ViewAction {
             override fun getConstraints(): Matcher<View> = isRoot()
             override fun getDescription(): String = "wait for $2 seconds"
