0c8ce684b79393d8730819624b17302ae5bb66c4
saityan <naminatosky@gmail.com>
2022-03-27 21:25:51 +0300

    tests repaired and a new test for Details totalCount added

--- app/src/androidTest/java/com/geekbrains/tests/automator/InitialTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/automator/InitialTest.kt
@@ -13,30 +13,20 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 @SdkSuppress(minSdkVersion = 18)
 class InitialTest {
-
-    //Контекст нам понадобится для запуска нужных экранов и получения packageName
     private val context = ApplicationProvider.getApplicationContext<Context>()
-
-    //Путь к классам нашего приложения, которые мы будем тестировать
     private val packageName = context.packageName
 
-    //Убеждаемся, что uiDevice не null
     @Test
     fun test_DeviceNotNull() {
-        //Класс UiDevice предоставляет доступ к вашему устройству.
-        //Именно через UiDevice вы можете управлять устройством, открывать приложения
-        //и находить нужные элементы на экране
         val uiDevice: UiDevice = UiDevice.getInstance(getInstrumentation())
         Assert.assertNotNull(uiDevice)
     }
 
-    //Проверяем, что приложение существует
     @Test
     fun test_AppPackageNotNull() {
         Assert.assertNotNull(packageName)
     }
 
-    //Проверяем, что Интент для запуска нашего приложения не null
     @Test
     fun test_MainActivityIntentNotNull() {
         val intent = context.packageManager.getLaunchIntentForPackage(packageName)
